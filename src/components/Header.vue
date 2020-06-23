dd28851ed75374592f37fa6db61d87abfcbc136d
Alex Abbott <a.abbott2596@gmail.com>
2020-06-22 20:36:09 -0600

    Removed Mock Data button

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,17 +1,12 @@
 <template>
     <div class="header">
         <span class="title">Easy Order Manager</span>
-        <span class="mockButton" @click="getMockData">Use Mock Data</span>
     </div>
 </template>
 
 <script>
 export default {
-    methods: {
-        getMockData() {
-            this.$store.dispatch('getMockData');
-        },
-    }
+
 }
 </script>
 
